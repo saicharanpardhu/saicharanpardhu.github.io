8a74aea36ff4237345dbc2e3543bedc6a8a206d8
saicharanpardhu <saicharanpardhu@gmail.com>
2017-09-07 12:21:56 +0530

    validation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
+    $("#contact-form").submit(function(e){
+                 document.getElementById("ajax-response1").style.display = "none";
+                 document.getElementById("ajax-response2").style.display = "none";
 
+                 var name= $("#c_name").val();
+                 var email= $("#c_email").val();
+                 var message= $("#c_message").val();
+                 if (email == ""){
+                  document.getElementById("ajax-response2").style.display = "block";
+                 }
+                 else{
+                  document.getElementById("ajax-response1").style.display = "block";
+                  $("#c_email").val('');
+                  $("#c_name").val('');
+                  $("#c_message").val('');
+                  
+                  // $.ajax({
+                  //     type: 'post',
+                  //     url: 'process.php',
+                  //    data: $('form').serialize(),
+                  //      success: function () {
+                  //       alert('form was submitted');
+                  //            }
+                  //       });
+
+
+                 }
+
+                e.preventDefault();
+                               });
+          
     /* ======= Scrollspy ======= */
    $('body').scrollspy({ target: '#page-nav-wrapper', offset: 100});
     
